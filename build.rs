b5fd687c6c6e00889f114f424ad6759e4ec9ba8c
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-09-28 08:33:31 +0200

    clippy

--- build.rs
+++ build.rs
@@ -9,12 +9,12 @@ fn main() {
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("endpoint_descriptor.bin"))
         .compile_protos(&["proto/endpoint_service.proto"], &["proto"])
-        .unwrap_or_else(|e| panic!("Failed to compile endpoint proto files: {}", e));
+        .unwrap_or_else(|e| panic!("Failed to compile endpoint proto files: {e}"));
 
     // Compile sentence service proto
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .file_descriptor_set_path(out_dir.join("sentence_descriptor.bin"))
         .compile_protos(&["proto/sentence_service.proto"], &["proto"])
-        .unwrap_or_else(|e| panic!("Failed to compile sentence proto files: {}", e));
+        .unwrap_or_else(|e| panic!("Failed to compile sentence proto files: {e}"));
 }
