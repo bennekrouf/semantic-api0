f04ac9decb375af48088a6ff64e49d530578ef5b
Bennekrouf <mohamed.bennekrouf@gmail.com>
2025-11-04 10:08:38 +0100

    Add log level

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     }
 
     let log_path = env::var("LOG_PATH_API0").unwrap_or_else(|_| "/var/log/api0.log".to_string());
-    init_logging!(&log_path, "api0", "semantic");
+    init_logging!(&log_path, "api0", "semantic", "info");
 
     let args: Vec<String> = std::env::args().collect();
     if args.len() <= 1 {
